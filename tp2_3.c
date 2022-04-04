e8dd42808c46ba548a4593b5b63dd669c02f42ef
Soria Alvaro <alvarosoria.universidad@gmail.com>
2022-04-04 18:50:04 -0300

    punto 3) terminado

---
+++ tp2_3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <time.h>
+
+#define N 5
+#define M 7
+
+int main() {
+	srand(time(NULL));
+	int i,j;
+	int mt[N][M], **ptr;
+	ptr = &mt;
+	for(i = 0; i < N; i++){
+		for(j = 0; j < M; j++){
+			*(ptr + (i * M + j))= 1 + rand()%100;
+
+			printf("%d		", *(ptr + (i * M + j)));
+		}
+		printf("\n");
+	}
+}
